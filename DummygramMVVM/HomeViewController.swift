8c1524cd4ce9c82ce4bd2360ab6784b729064c21
Bagas Ilham <rabbanilham@gmail.com>
2022-08-04 19:57:55 +0700

    add project files and pod init

---
+++ DummygramMVVM/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  DummygramMVVM
+//
+//  Created by Bagas Ilham on 04/08/22.
+//
+
+import UIKit
+
+final class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
